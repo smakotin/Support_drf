1e061a53210c45a6d14334a971f870b600f73ae5
Andrey Smakotin <slamfromm@gmail.com>
2022-06-18 22:40:15 +0300

    added JWTAuthentication to settings.py

--- support/support/settings.py
+++ support/support/settings.py
@@ -94,6 +94,12 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
